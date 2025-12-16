9e5fc2138fbc51990e8e7e958db1a9f2a3e5f96b
Zookegger <nguyenductrung11122004@gmail.com>
2025-12-16 12:02:32 +0700

    feat: add missing plugin declarations in build.gradle.kts for Android application

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
 allprojects {
     repositories {
         google()
