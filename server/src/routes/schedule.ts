103a51f7cdf9e2cc16f21c3ff528ccbfade4ff2a
Zookegger <nguyenductrung11122004@gmail.com>
2026-01-07 02:49:13 +0700

    fix: change POST to GET for user-specific schedule retrieval endpoint

--- server/src/routes/schedule.ts
+++ server/src/routes/schedule.ts
@@ -14,7 +14,7 @@ const scheduleRouter = Router();
 scheduleRouter.get('/schedules', ScheduleController.listSchedules);
 scheduleRouter.get('/schedules/search', searchScheduleValidator, ScheduleController.searchSchedules);
 scheduleRouter.get('/schedules/:id', getScheduleValidator, ScheduleController.getSchedule);
-scheduleRouter.post('/schedules/user/:userId', getScheduleByUserValidator, ScheduleController.getSchedulebyUser);
+scheduleRouter.get('/schedules/user/:userId', getScheduleByUserValidator, ScheduleController.getSchedulebyUser);
 scheduleRouter.post('/schedules', createScheduleValidator, ScheduleController.createSchedule);
 scheduleRouter.put('/schedules/:id', updateScheduleValidator, ScheduleController.updateSchedule);
 scheduleRouter.delete('/schedules/:id', deleteScheduleValidator, ScheduleController.deleteSchedule);
